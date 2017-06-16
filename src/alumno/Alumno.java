90c4522f5fd98a96f55b192417a1b35aa7aa7139
programador3 <programador3@programador3>
2017-06-16 20:13:45 -0300

    Se agrego unos metodos que devuelven las previas del alumno

--- src/alumno/Alumno.java
+++ src/alumno/Alumno.java
@@ -7,17 +7,25 @@ import javax.persistence.*;
 import org.openxava.annotations.*;
 
 import cursado.*;
+import curso.*;
+import examen.*;
+import materia.*;
+import planilla.*;
 import util.*;
 
 @Entity
 @Views({ @View(members = "dni;nombreYApellido;direccion;telefono;celular;mail"),
 		@View(name = "Simple", members = "documento;nombreYApellido"),
+		@View(name = "Simple2", members = "dni,nombreYApellido"),
 		@View(name = "SimpleCursado", members = "dni;nombreYApellido") })
 @Tabs({ @Tab(properties = "dni,nombreYApellido,telefono,celular,mail,curso") })
 public class Alumno extends Persona {
 
 	@OneToMany(mappedBy = "alumno", cascade = CascadeType.ALL)
 	private List<Cursado> cursados;
+
+	@OneToMany(mappedBy = "alumno")
+	private List<Examen> examenesFinales;
 
 	@Stereotype("ETIQUETA")
 	public Long getDocumento() {
@@ -38,6 +46,33 @@ public class Alumno extends Persona {
 		String retorno = "Sin Curso";
 		if (devolverCursoHabilitadoDelAnioActual() != null) {
 			retorno = devolverCursoHabilitadoDelAnioActual().getCurso().devolverInformacion();
+		}
+		return retorno;
+	}
+
+	public List<MateriaPlanilla> devolverPrevias() {
+		ArrayList<MateriaPlanilla> previas = new ArrayList<MateriaPlanilla>();
+		for (Cursado cursado : cursados) {
+			previas.addAll(cursado.getPlanilla().devolverPrevias());
+		}
+
+		for (Examen examen : examenesFinales) {
+			if (examen.getAprobo()) {
+				// if(previas.contains(examen.getMateriaPlanilla())){
+				previas.remove(examen.getMateriaPlanilla());
+				// }
+			}
+		}
+		return previas;
+	}
+
+	public String codigosDePrevias() {
+		String retorno = "'ae'";
+		for (MateriaPlanilla materiaPlanilla : devolverPrevias()) {
+			retorno = retorno + "'" + materiaPlanilla.getId() + "',";
+		}
+		if (!retorno.equals("'ae'")) {
+			retorno = retorno.substring(4, retorno.length() - 1);
 		}
 		return retorno;
 	}
